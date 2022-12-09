bc4b5a6a9c036583152cf60a0952d2d13f9b8d82
N1kOk <n1k_ok@bk.ru>
2022-12-09 23:38:53 +0500

    refactor: add link component

---
+++ src/components/AppLink.vue
@@ -0,0 +1,35 @@
+<template>
+	<component
+		class="link"
+		:class="classes"
+		:is="componentName"
+		:to="(!isExternalLink || undefined) && to"
+		:href="to"
+	>
+		<slot/>
+	</component>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+
+interface Props {
+	to?: string
+	isExternalLink?: boolean
+	color?: 'aqua'
+}
+
+const { to, isExternalLink, color } = defineProps<Props>()
+
+const componentName = computed(() => isExternalLink ? 'a' : 'RouterLink')
+
+const classes = computed(() => ({
+	'text-aqua underline underline-offset-1': color === 'aqua',
+}))
+</script>
+
+<style scoped>
+.link {
+	@apply flex items-center whitespace-nowrap
+}
+</style>
