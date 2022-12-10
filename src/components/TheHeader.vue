6a1162bb393d839cc264ef718007cc0bf0fa0272
N1kOk <n1k_ok@bk.ru>
2022-12-10 18:39:49 +0500

    refactor: extract classes to scss

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,16 +1,13 @@
 <template>
-	<header class="flex fixed px-3 py-1 w-full min-h-[70px] bg-blue md:px-[50px] shadow-xl z-50">
-		<nav class="flex justify-between w-full my-auto bg-inherit md:space-x-4">
-			<AppLink class="flex items-center min-w-max" to="/">
+	<header class="header">
+		<nav class="header__nav">
+			<AppLink class="min-w-max flex items-center" to="/">
 				<img class="h-9" src="/assets/images/logo.png" alt="DZ-Helper">
-				<span class="ml-1 font-bold whitespace-nowrap text-[1.5rem]">DZ-Helper</span>
+				<span class="ml-1 text-3xl font-bold whitespace-nowrap">DZ-Helper</span>
 			</AppLink>
 
 			<input class="hidden" id="header__menu-toggle" type="checkbox"/>
-			<label
-				class="w-[35px] h-[27px] m-auto mr-0 p-[10px] box-content flex flex-col justify-between md:hidden"
-				for="header__menu-toggle"
-			>
+			<label class="header__burger" for="header__menu-toggle">
 				<span
 					v-for="i in 3"
 					class="transition-transform h-[3px] rounded-2xl bg-white"
@@ -18,9 +15,7 @@
 				/>
 			</label>
 
-			<div
-				class="header__links text-sm md:text-[16px] transition-transform will-change-transform [transform:scaleY(0)] origin-top absolute top-[70px] left-0 flex flex-wrap flex-col items-center w-full pb-5 bg-inherit md:static md:flex-row md:justify-end md:scale-y-100 md:p-0 md:my-auto md:space-x-4 md:w-max font-medium [&>*:not(:last-child)]:min-w-full md:[&>*]:!min-w-max"
-			>
+			<div class="header__links">
 				<AppButton to="/vklogin.php" is-external-link>
 					<IconLogin class="w-4 mr-1"/>
 					<span>Авторизоваться</span>
@@ -43,11 +38,9 @@
 					<div class="flex items-center cursor-pointer">
 						<IconInfo class="min-w-[1rem] mr-1"/>
 						<span>Расширения</span>
-						<IconArrowDown class="min-w-[0.75rem]"/>
+						<IconArrowDown class="min-w-[1rem]"/>
 					</div>
-					<div
-						class="group-hover:scale-y-100 md:absolute md:scale-y-0 top-[50px] left-0 flex flex-col p-2 rounded-b-2xl md:space-y-1 space-y-2 origin-top transition-transform bg-blue will-change-transform text-[0.9em] w-full"
-					>
+					<div class="header__extensions">
 						<AppLink to="/webgramotei.html">Веб-Грамотей+</AppLink>
 						<AppLink to="/reshu.html">РешуОГЭ/ЕГЭ+</AppLink>
 						<AppLink to="/yaklass.html">ЯКласс+</AppLink>
@@ -76,3 +69,37 @@ import IconInfo from '@/components/icons/IconInfo.vue'
 import IconArrowDown from '@/components/icons/IconArrowDown.vue'
 import IconDownload from '@/components/icons/IconDownload.vue'
 import IconUser from '@/components/icons/IconUser.vue'</script>
+
+<style scoped lang="scss">
+.header {
+	@apply
+	fixed w-full min-h-[70px] px-3 py-1 flex bg-blue shadow-xl z-50
+	lg:px-[50px];
+
+	&__nav {
+		@apply
+		w-full my-auto flex justify-between bg-inherit
+		lg:space-x-4;
+	}
+
+	&__burger {
+		@apply
+		w-[35px] h-[27px] p-3 m-auto mr-0 box-content flex flex-col justify-between
+		lg:hidden;
+	}
+
+	&__links {
+		@apply
+		absolute w-full left-0 top-[70px] pb-5 flex flex-col items-center flex-wrap text-sm font-medium bg-inherit
+		transition-transform will-change-transform scale-y-0 origin-top
+
+		lg:static lg:w-max lg:p-0 lg:my-auto lg:flex-row lg:justify-end lg:space-x-4
+		lg:scale-y-100;
+	}
+
+	&__extensions {
+		@apply
+		group-hover:scale-y-100 lg:absolute lg:scale-y-0 top-[50px] left-0 flex flex-col p-2 rounded-b-2xl lg:space-y-1 space-y-2 origin-top transition-transform bg-blue will-change-transform text-[0.9em] w-full;
+	}
+}
+</style>
