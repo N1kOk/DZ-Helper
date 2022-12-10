f31271ea1aecb6a6abda8d98c8a3c96f0a942993
N1kOk <n1k_ok@bk.ru>
2022-12-10 21:46:11 +0500

    fix: footer

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,8 +1,17 @@
 <template>
 	<footer class="relative flex p-1 bg-blue min-h-[70px] text-xs leading-5 font-light shadow-2xl shadow-black z-50">
-		<div class="m-auto md:[&>br]:hidden">
-			© Copyright 2022 DZ-Service<br>&nbsp;<span class="hidden md:inline">|</span>&nbsp;<a href="#" target="_blank" rel="nofollow">Группа ВКонтакте</a>
-			<br><RouterLink to="/privacy.html" target="_blank" rel="nofollow">Политика конфиденциальности</RouterLink>
+		<div class="m-auto lg:[&>br]:hidden max-lg:[&>span]:hidden">
+			© Copyright 2023 DZ-Helper
+			<br>
+			&nbsp;<span>|</span>&nbsp;
+			<a href="#" target="_blank" rel="nofollow">Группа ВКонтакте</a>
+			&nbsp;<span>|</span>&nbsp;
+			<br>
+			<AppLink to="/privacy.html" rel="nofollow">Политика конфиденциальности</AppLink>
 		</div>
 	</footer>
 </template>
+
+<script setup lang="ts">
+import AppLink from '@/components/AppLink.vue'
+</script>
