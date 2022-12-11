6aeedff6e19a9a32b3eea3163ca6742979a4074a
N1kOk <n1k_ok@bk.ru>
2022-12-11 16:04:26 +0500

    chore: edit content

--- src/views/DownloadView.vue
+++ src/views/DownloadView.vue
@@ -1,21 +1,130 @@
 <template>
-	<div class="flex flex-wrap justify-center">
-		<div class="flex flex-col py-4 p-8 space-y-8 bg-white/5 rounded-3xl shadow-xl shadow-black/25">
-			<div class="text-2xl font-bold">ВебГрамотей</div>
-			<div class="flex flex-1 justify-between space-x-4 text-left">
-				<div>
-					<div>1 слово</div>
-					<div>1 предложение</div>
-				</div>
-				<div class="text-yellow">
-					<div>1 коин</div>
-					<div>2 коина</div>
+	<h1 class="text-4xl font-bold">Расширения</h1>
 
-<!--					<div>2 коина</div>-->
-<!--					<div>3 коина</div>-->
-				</div>
+	<div class="grid grid-cols-[repeat(auto-fit,minmax(18rem,1fr))] gap-4">
+		<div
+			v-for="card in cards"
+			class="flex flex-col items-center gap-8 p-4 rounded-3xl border-2 shadow-xl bg-white/5"
+		>
+			<div class="flex justify-between items-center w-full gap-2">
+				<span class="text-2xl font-bold">{{ card.title }}</span>
+				<component :is="card.icon" class="w-12"/>
 			</div>
-			<RouterLink class="btn" to="/webgramotei">Запустить</RouterLink>
+
+			<div class="flex justify-between w-full gap-2">
+				<AppButton
+					:to="card.links.instruction"
+					target="_blank"
+					rel="nofollow"
+				>
+					Инструкция
+				</AppButton>
+
+				<AppButton
+					:to="card.links.setup"
+					target="_blank"
+					rel="nofollow"
+					color="green"
+				>
+					Установить
+				</AppButton>
+			</div>
 		</div>
+
+	</div>
+	<hr>
+	<div class="ads mx-auto">
+		<!-- Yandex.RTB R-A-489599-1 -->
+		<div id="yandex_rtb_R-A-489599-1" style="width: fit-content;"></div>
+		<component is="script" type="text/javascript">
+			(function (w, d, n, s, t) {
+			if (window.bBlockAds) return;
+			w[n] = w[n] || [];
+			w[n].push(function () {
+			Ya.Context.AdvManager.render({
+			blockId: "R-A-489599-1",
+			renderTo: "yandex_rtb_R-A-489599-1",
+			async: true
+			});
+			});
+			t = d.getElementsByTagName("script")[0];
+			s = d.createElement("script");
+			s.type = "text/javascript";
+			s.src = "//an.yandex.ru/system/context.js";
+			s.async = true;
+			t.parentNode.insertBefore(s, t);
+			})(this, this.document, "yandexContextAsyncCallbacks");
+		</component>
 	</div>
 </template>
+
+<script setup lang="ts">
+import type { Component } from 'vue'
+import AppButton from '@/components/AppButton.vue'
+import IconRocket from '@/components/icons/IconRocket.vue'
+import IconTarget from '@/components/icons/IconTarget.vue'
+import IconLike from '@/components/icons/IconLike.vue'
+import IconCheck from '@/components/icons/IconCheck.vue'
+import IconMath from '@/components/icons/IconMath.vue'
+import IconSum from '@/components/icons/IconSum.vue'
+
+interface Card {
+	icon: Component
+	title: string
+	links: {
+		setup: string
+		instruction: string
+	}
+}
+
+const cards: Card[] = [
+	{
+		icon: IconCheck,
+		title: 'Веб-Грамотей+',
+		links: {
+			setup: 'https://chrome.google.com/webstore/detail/wg-stealth/gmojcbijaafhccilpieladabaiendmmd',
+			instruction: 'https://vk.com/@dzhelper1-wg-stealh',
+		},
+	},
+	{
+		icon: IconRocket,
+		title: 'Учи.ру+',
+		links: {
+			setup: 'https://chrome.google.com/webstore/detail/uchihelper/fpdmlanpbgokpkcmkojldkjlajcnajko',
+			instruction: 'https://vk.com/@dzhelper1-uchiruplus',
+		},
+	},
+	{
+		icon: IconTarget,
+		title: 'ЯКласс+',
+		links: {
+			setup: 'https://chrome.google.com/webstore/detail/%D0%BE%D1%82%D0%B2%D0%B5%D1%82%D1%8B-%D0%BD%D0%B0-%D1%8F%D0%BA%D0%BB%D0%B0%D1%81%D1%81-%D1%8F%D0%BA%D0%BB%D0%B0%D1%81%D1%81%20/gkdimpanhbihidflnpedojnljpapgmbe?hl=ru',
+			instruction: 'https://vk.com/@dzhelper1-yaklassplus',
+		},
+	},
+	{
+		icon: IconLike,
+		title: 'РЭШ+',
+		links: {
+			setup: 'https://chrome.google.com/webstore/detail/%D0%BE%D1%82%D0%B2%D0%B5%D1%82%D1%8B-%D0%BD%D0%B0-%D1%80%D1%8D%D1%88-%D1%80%D1%8D%D1%88%20/dfojlcjcmgijemlijicemapllecfnmpk?hl=ru',
+			instruction: 'https://vk.com/@dzhelper1-reshplus',
+		},
+	},
+	{
+		icon: IconMath,
+		title: 'UzTest+',
+		links: {
+			setup: 'https://chrome.google.com/webstore/detail/%D0%BE%D1%82%D0%B2%D0%B5%D1%82%D1%8B-%D0%BA-uztestru-uztest%20/aeookmmgmjgnmfoaiemejdoailigdgag',
+			instruction: 'https://vk.com/@dzhelper1-uztestplus',
+		},
+	},
+	{
+		icon: IconSum,
+		title: 'РешуОГЭ/ЕГЭ+',
+		links: {
+			setup: 'https://chrome.google.com/webstore/detail/%D0%BE%D1%82%D0%B2%D0%B5%D1%82%D1%8B-%D0%BA-%D1%80%D0%B5%D1%88%D1%83-%D0%BE%D0%B3%D1%8D-%D0%B8-%D1%80%D0%B5%D1%88%D1%83/ilocfjnmkngcmoonmlpmdiigiddlloea?hl=ru',
+			instruction: 'https://vk.com/@dzhelper1-reshugiaplus',
+		},
+	},
+]
+</script>
