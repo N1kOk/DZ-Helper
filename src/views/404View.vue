bf77b42e0957209a51b56de8e9173c57a9fffd4b
N1kOk <n1k_ok@bk.ru>
2022-12-12 18:21:25 +0500

    fix: center content

--- src/views/404View.vue
+++ src/views/404View.vue
@@ -1,6 +1,11 @@
 <template>
-	<div class="space-y-8">
-		<div class="mx-auto text-5xl font-bold">Ошибка 404</div>
-		<div class="mx-auto text-xl">Страница не найдена</div>
-	</div>
+	<AppSection class="min-h-[calc(100vh-70px)]" is-stretched>
+		<div class="space-y-8">
+			<div class="mx-auto text-3xl font-bold">Ошибка 404</div>
+			<div class="mx-auto text-xl">Страница не найдена</div>
+		</div>
+	</AppSection>
 </template>
+
+<script setup lang="ts">
+import AppSection from '@/components/AppSection.vue'</script>
