9121a4b3a35f961acc78b1519c3d5178b556a5d7
N1kOk <n1k_ok@bk.ru>
2022-12-13 13:38:56 +0500

    chore: add new icons

---
+++ src/components/icons/IconMouse.vue
@@ -0,0 +1,8 @@
+<template>
+	<svg viewBox="4 1 16 22" stroke-width="2" stroke="currentColor"
+	     fill="none" stroke-linecap="round" stroke-linejoin="round">
+		<path stroke="none" d="M0 0h24v24H0z"/>
+		<rect x="7" y="4" width="10" height="16" rx="4"/>
+		<line x1="12" y1="8" x2="12" y2="11"/>
+	</svg>
+</template>
