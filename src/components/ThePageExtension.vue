13210cbb5eb549dea3dc942a224aeccc713a5027
N1kOk <n1k_ok@bk.ru>
2022-12-14 23:14:47 +0500

    refactor: minor improvements

---
+++ src/components/ThePageExtension.vue
@@ -0,0 +1,150 @@
+<template>
+	<TheSectionMain class="lg:min-h-screen">
+		<template #title>
+			{{ props.title }}
+		</template>
+		<template #description>
+			<slot name="description"/>
+		</template>
+	</TheSectionMain>
+
+	<AppSection :style="props.backgroundStyle">
+		<div class="flex max-lg:flex-col gap-x-4 gap-y-8 [&>*]:flex-1">
+			<div class="flex flex-col space-y-8 text-left">
+				<h2 class="text-3xl font-bold">Смотрите сами</h2>
+				<p>
+					<slot name="fullDescription"/>
+				</p>
+				<p>
+					Инструкцию можно посмотреть
+					<AppLink
+						:to="props.instructionUrl"
+						target="_blank"
+						rel="nofollow"
+						color="aqua"
+					>
+						здесь
+					</AppLink>
+				</p>
+			</div>
+			<div class="flex">
+				<img
+					class="m-auto object-contain rounded-3xl shadow-xl"
+					:src="props.imageUrl"
+					:alt="props.title + ' расширение'"
+				>
+			</div>
+		</div>
+		<div class="ads mx-auto">
+			<!-- Yandex.RTB R-A-489599-1 -->
+			<div id="yandex_rtb_R-A-489599-1" style="width: fit-content;"></div>
+			<component is="script" type="text/javascript">
+				(function (w, d, n, s, t) {
+				if (window.bBlockAds) return;
+				w[n] = w[n] || [];
+				w[n].push(function () {
+				Ya.Context.AdvManager.render({
+				blockId: 'R-A-489599-1',
+				renderTo: 'yandex_rtb_R-A-489599-1',
+				async: true,
+				});
+				});
+				t = d.getElementsByTagName('script')[0];
+				s = d.createElement('script');
+				s.type = 'text/javascript';
+				s.src = '//an.yandex.ru/system/context.js';
+				s.async = true;
+				t.parentNode.insertBefore(s, t);
+				})(this, this.document, 'yandexContextAsyncCallbacks');
+			</component>
+		</div>
+	</AppSection>
+
+	<AppSection class="max-lg:hidden">
+		<h2 class="text-5xl font-bold uppercase">Как начать?</h2>
+	</AppSection>
+
+	<AppSection class="text-xl text-black font-medium bg-white max-lg:hidden">
+		<div class="flex flex-wrap justify-evenly gap-4">
+			<div class="flex items-center gap-1">
+				<IconCursor class="h-12 text-gray-300"/>
+				<span>Открой</span>
+			</div>
+			<div class="flex items-center gap-1">
+				<IconMouse class="h-12 text-gray-300"/>
+				<span>Попробуй</span>
+			</div>
+			<div class="flex items-center gap-1">
+				<IconSmile class="h-12 text-gray-300"/>
+				<span>Пользуйся</span>
+			</div>
+		</div>
+	</AppSection>
+
+	<AppSection>
+		<div class="grid grid-cols-[repeat(auto-fit,minmax(11rem,1fr))] gap-4">
+			<slot name="prices"/>
+		</div>
+	</AppSection>
+
+	<AppSection>
+		<AppCard class="flex flex-col items-center w-full max-w-[30rem] p-8 mx-auto space-y-6">
+			<IconDevices class="w-24"/>
+			<h2 class="text-3xl font-bold">{{ props.title }}</h2>
+			<div class="space-y-2">
+				<p>Расширение подходит для большинства браузеров (Chrome, Яндекс.Браузер, Opera...)</p>
+				<p>
+					Для установки расширения на смартфон воспользуйтесь
+					<AppLink
+						:to="props.instructionUrl"
+						target="_blank"
+						rel="nofollow"
+						color="aqua"
+					>
+						инструкцией
+					</AppLink>
+				</p>
+			</div>
+
+			<AppButton
+				class="mx-auto"
+				:to="Link.Download"
+				:color="ButtonColor.Green"
+				:size="ButtonSize.Large"
+			>
+				Перейти к расширениям
+			</AppButton>
+		</AppCard>
+	</AppSection>
+
+	<AppSection>
+		<p class="text-sm text-gray-300">
+			Небольшая оптимизация контента:
+			<br>
+			<slot name="keywords"/>
+		</p>
+	</AppSection>
+</template>
+
+<script setup lang="ts">
+import TheSectionMain from '@/components/TheSectionMain.vue'
+import AppSection from '@/components/AppSection.vue'
+import AppLink from '@/components/AppLink.vue'
+import IconCursor from '@/components/icons/IconCursor.vue'
+import IconMouse from '@/components/icons/IconMouse.vue'
+import IconSmile from '@/components/icons/IconSmile.vue'
+import AppButton from '@/components/AppButton.vue'
+import { Link } from '@/router'
+import IconDevices from '@/components/icons/IconDevices.vue'
+import AppCard from '@/components/AppCard.vue'
+import { ButtonColor, ButtonSize } from '@/utils/button'
+
+interface Props {
+	title: string
+	instructionUrl: string
+	backgroundStyle: string
+	imageUrl: string
+}
+
+const props = defineProps<Props>()
+</script>
--- src/views/extensions/WebGramoteiView.vue
+++ src/views/extensions/WebGramoteiView.vue
@@ -1,99 +1,29 @@
 <template>
-	<TheMainSection class="lg:min-h-screen">
-		<template #title>
-			Веб-Грамотей
-		</template>
+	<ThePageExtension
+		title="Веб-Грамотей"
+		instruction-url="https://vk.com/@dzhelper1-wgs"
+		background-style="background:linear-gradient(45deg,#46d11b,#149dcc)"
+		image-url="/assets/images/wg.png"
+	>
 		<template #description>
 			С нашим сервисом для браузера вы легко сможете выполнять задания на сайте <b>Веб-Грамотей</b>
 			<br class="max-lg:hidden">
 			и всегда получать хорошие оценки за домашние задания
 		</template>
-	</TheMainSection>
-
-	<AppSection class="[background:linear-gradient(45deg,#46d11b,#149dcc)]">
-		<div class="flex max-lg:flex-col gap-x-4 gap-y-8 [&>*]:flex-1">
-			<div class="flex flex-col space-y-8 text-left">
-				<h2 class="text-3xl font-bold">Смотрите сами</h2>
-				<p>
-					Быстрое полуавтоматическое выполнение заданий с вашим участием. Наша программа знает все
-					<b>ответы</b> на <b>Веб-Грамотей</b>, и вам даже не нужно прикладывать особых усилий,
-					чтобы всё работало.
-					<br>
-					Просто откройте Веб-Грамотей и расширение само выделит верный ответ.
-					<br>
-					Кроме того, что наш сервис содержит все <b>ответы Веб-Грамотей</b>, он может находить
-					ответы на специальные задания для 9-го класса, так как ответы <b>ОРФО-9</b>
-					и упражнений на пунктуацию так же включены в базу данных программы.
-				</p>
-				<p>
-					Инструкцию для словаря Веб-Грамотей можно посмотреть
-					<AppLink
-						to="https://vk.com/@dzhelper1-wgs"
-						target="_blank"
-						rel="nofollow"
-						color="aqua"
-					>
-						здесь
-					</AppLink>
-				</p>
-			</div>
-			<div class="flex">
-				<img
-					class="m-auto object-contain rounded-3xl shadow-xl"
-					src="/assets/images/wg.png"
-					alt="Веб-Грамотей расширение"
-				>
-			</div>
-		</div>
-		<div class="ads mx-auto">
-			<!-- Yandex.RTB R-A-489599-1 -->
-			<div id="yandex_rtb_R-A-489599-1" style="width: fit-content;"></div>
-			<component is="script" type="text/javascript">
-				(function (w, d, n, s, t) {
-				if (window.bBlockAds) return;
-				w[n] = w[n] || [];
-				w[n].push(function () {
-				Ya.Context.AdvManager.render({
-				blockId: 'R-A-489599-1',
-				renderTo: 'yandex_rtb_R-A-489599-1',
-				async: true,
-				});
-				});
-				t = d.getElementsByTagName('script')[0];
-				s = d.createElement('script');
-				s.type = 'text/javascript';
-				s.src = '//an.yandex.ru/system/context.js';
-				s.async = true;
-				t.parentNode.insertBefore(s, t);
-				})(this, this.document, 'yandexContextAsyncCallbacks');
-			</component>
-		</div>
-	</AppSection>
-
-	<AppSection class="max-lg:hidden">
-		<h2 class="text-5xl font-bold uppercase">Как начать?</h2>
-	</AppSection>
-
-	<AppSection class="text-xl text-black font-medium bg-white max-lg:hidden">
-		<div class="flex flex-wrap justify-evenly gap-4">
-			<div class="flex items-center gap-1">
-				<IconCursor class="h-12 text-gray-300"/>
-				<span>Открой</span>
-			</div>
-			<div class="flex items-center gap-1">
-				<IconMouse class="h-12 text-gray-300"/>
-				<span>Попробуй</span>
-			</div>
-			<div class="flex items-center gap-1">
-				<IconSmile class="h-12 text-gray-300"/>
-				<span>Пользуйся</span>
-			</div>
-		</div>
-	</AppSection>
-
-	<AppSection>
-		<div class="grid grid-cols-[repeat(auto-fit,minmax(175px,1fr))] gap-4">
+		<template #fullDescription>
+			Быстрое полуавтоматическое выполнение заданий с вашим участием. Наша программа знает все
+			<b>ответы</b> на <b>Веб-Грамотей</b>, и вам даже не нужно прикладывать особых усилий,
+			чтобы всё работало.
+			<br>
+			Просто откройте Веб-Грамотей и расширение само выделит верный ответ.
+			<br>
+			Кроме того, что наш сервис содержит все <b>ответы Веб-Грамотей</b>, он может находить
+			ответы на специальные задания для 9-го класса, так как ответы <b>ОРФО-9</b>
+			и упражнений на пунктуацию так же включены в базу данных программы.
+		</template>
+		<template #prices>
 			<AppCardPrice
+				class="gap-6"
 				:cost="0"
 				description="Попробуй наш продукт в действии и убедись в его качестве"
 				:button="{ to: Link.Download, color: ButtonColor.Green, text: 'Испытать' }"
@@ -107,6 +37,7 @@
 			</AppCardPrice>
 
 			<AppCardPrice
+				class="gap-6"
 				:cost="0"
 				description="Есть возможность заработать коины совершенно бесплатно"
 				:button="{ to: Link.Stock, color: ButtonColor.Blue, text: 'Заработать' }"
@@ -126,11 +57,11 @@
 			</AppCardPrice>
 
 			<AppCardPrice
-				:cost="0"
+				class="gap-6"
+				:cost="70"
 				description="Приобрети коины сразу и ни в чём себе не отказывай!"
 				:button="{ to: Link.Buy, color: ButtonColor.Yellow, text: 'Купить' }"
 			>
-
 				<p>100 слов</p>
 				<p class="text-sm text-gray-300">&nbsp;или</p>
 				<div class="flex flex-col w-max text-center">
@@ -138,64 +69,21 @@
 					<span class="text-xs -mt-1 text-gray-300">(пунктуация)</span>
 				</div>
 			</AppCardPrice>
-		</div>
-	</AppSection>
-
-	<AppSection>
-		<AppCard class="flex flex-col items-center w-full max-w-[30rem] p-8 mx-auto space-y-6">
-			<IconDevices class="w-24"/>
-			<h2 class="text-3xl font-bold">Веб-Грамотей</h2>
-			<div class="space-y-2">
-				<p>Расширение подходит для большинства браузеров (Chrome, Яндекс.Браузер, Opera...)</p>
-				<p>
-					Для установки расширения на смартфон воспользуйтесь
-					<AppLink
-						to="https://vk.com/@dzhelper1-wgs"
-						target="_blank"
-						rel="nofollow"
-						color="aqua"
-					>
-						инструкцией
-					</AppLink>
-				</p>
-			</div>
-
-			<AppButton
-				class="mx-auto"
-				:to="Link.Download"
-				color="green"
-				size="xl"
-			>
-				Перейти к расширениям
-			</AppButton>
-		</AppCard>
-	</AppSection>
-
-	<AppSection>
-		<p class="text-sm text-gray-300">
-			Небольшая оптимизация контента:
-			<br>
+		</template>
+		<template #keywords>
 			Web-Gramotei, взлом веб-грамотей, веб-грамотей ответы, взлом орфо 9, как взломать веб-грамотей,
 			как сделать быстро
 			веб-грамотей, как взломать орфо 9, как быстро сделать орфо 9, веб грамотей решение, решение орфо
 			9, орфо9, орфо тренажер
 			9, орфо тренажер 9 ответы, орфо 9 ответы на упражнения, программа орфо 9, ответы на орфо
 			тренажер 9 класс
-		</p>
-	</AppSection>
+		</template>
+	</ThePageExtension>
 </template>
 
 <script setup lang="ts">
-import TheMainSection from '@/components/TheSectionMain.vue'
-import AppSection from '@/components/AppSection.vue'
-import AppLink from '@/components/AppLink.vue'
-import IconCursor from '@/components/icons/IconCursor.vue'
-import IconMouse from '@/components/icons/IconMouse.vue'
-import IconSmile from '@/components/icons/IconSmile.vue'
-import AppButton from '@/components/AppButton.vue'
-import { Link } from '@/router'
-import IconInfinity from '@/components/icons/IconInfinity.vue'
-import IconDevices from '@/components/icons/IconDevices.vue'
+import ThePageExtension from '@/components/ThePageExtension.vue'
 import AppCardPrice from '@/components/AppCardPrice.vue'
-import AppCard from '@/components/AppCard.vue'
-import { ButtonColor } from '@/utils/button'</script>
+import IconInfinity from '@/components/icons/IconInfinity.vue'
+import { ButtonColor } from '@/utils/button'
+import { Link } from '@/router'</script>
