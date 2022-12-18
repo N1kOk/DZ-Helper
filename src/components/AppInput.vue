cfafb76805a55e1e7d2ee72959d68795ea9b210b
N1kOk <n1k_ok@bk.ru>
2022-12-18 20:55:38 +0500

    feat: add input component

---
+++ src/components/AppInput.vue
@@ -0,0 +1,20 @@
+<template>
+	<input
+		:value="modelValue"
+		@input="$emit('update:modelValue', $event.target.value)"
+		type="text"
+		class="input"
+	>
+</template>
+
+<script setup lang="ts">
+defineProps(['modelValue'])
+</script>
+
+<style scoped lang="scss">
+.input {
+	@apply
+	inline-block text-center text-sm text-black font-light border border-black/50 rounded-full
+	focus:outline-2 outline-blue;
+}
+</style>
