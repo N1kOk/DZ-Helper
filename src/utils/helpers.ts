d970293e2ca7a5ae648d4195e586d78da4dcb058
N1kOk <n1k_ok@bk.ru>
2022-12-19 19:37:08 +0500

    refactor: minor improvements

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,6 +1,27 @@
+import Swal from 'sweetalert2'
+
 export const isDev = location.hostname === '127.0.0.1'
 
 export const baseUrl = isDev ? 'https://dz-helper.ru' : ''
+
+export async function get<T extends string = string>(url: string) {
+	return (<T>(await (await fetch(baseUrl + url)).text()).trim())
+}
+
+export async function copyRefLink(event: MouseEvent) {
+	const url = (<HTMLSpanElement>event.target).textContent
+	if (url) {
+		await copyToClipboard(url)
+		await Toast.fire({
+			icon: 'success',
+			title: 'Реферальная ссылка скопирована',
+		})
+	}
+}
+
+export function copyToClipboard(text: string) {
+	return navigator.clipboard.writeText(text)
+}
 
 export function debounce(this: any, fn: (...args: any[]) => any, ms = 1000) {
 	let timeoutId: number
@@ -9,3 +30,17 @@ export function debounce(this: any, fn: (...args: any[]) => any, ms = 1000) {
 		timeoutId = setTimeout(() => fn.call(this, ...args), ms)
 	}
 }
+
+export const Toast = Swal.mixin({
+	toast: true,
+	position: 'bottom-start',
+	showConfirmButton: false,
+	timerProgressBar: true,
+	timer: 3000,
+	width: 'max-content',
+	didOpen: (toast) => {
+		toast.addEventListener('mouseenter', Swal.stopTimer)
+		toast.addEventListener('mouseleave', Swal.resumeTimer)
+	},
+})
+
