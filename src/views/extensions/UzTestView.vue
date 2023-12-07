74575cd6501f56b37bf8357665387e203fb5ad22
N1kOk <n1k_ok@bk.ru>
2023-12-07 15:32:25 +0500

    feat: add domashky.net to uztest page

--- src/views/extensions/UzTestView.vue
+++ src/views/extensions/UzTestView.vue
@@ -1,5 +1,6 @@
 <template>
 	<ThePageExtension
+    is-disabled
 		title="UzTest"
 		instruction-url="https://vk.com/@dzhelper1-uztestplus"
 		background-style="background:linear-gradient(45deg,rgb(10,165,218),rgb(38,148,38))"
@@ -11,42 +12,10 @@
 			Кроме того, вы всегда сможете подробно увидеть, как был получен <b>ответ</b>
 		</template>
 		<template #fullDescription>
-			Быстрый поиск <b>ответов</b> на задания <b>UzTest</b> в нашей базе заданий.
-			Наше расширение легко покажет вам верный
-			<b>ответ</b> на задание, а так же поможет понять, как был получен финальный
-			<b>ответ</b>. Просто нажмите на кнопку, чтобы наше расширение нашло <b>ответ</b> на любой вопрос в
-			<b>UzTest</b> и наслаждайтесь временем, которое вы сэкономили на выполнении.
-		</template>
-		<template #prices>
-			<AppCardPrice
-				class="gap-6"
-				:cost="0"
-				description="Попробуй наш продукт в действии и убедись в его качестве"
-				:button="{ to: Link.Download, color: ButtonColor.Green, text: 'Испытать' }"
-			>
-				<p>3 решения</p>
-			</AppCardPrice>
-
-			<AppCardPrice
-				class="gap-6"
-				:cost="0"
-				description="Есть возможность заработать коины совершенно бесплатно"
-				:button="{ to: Link.Stock, color: ButtonColor.Blue, text: 'Заработать' }"
-			>
-				<div class="flex items-center text-left">
-					<IconInfinity class="w-6" stroke-width="1"/>
-					<span>&nbsp;решений</span>
-				</div>
-			</AppCardPrice>
-
-			<AppCardPrice
-				class="gap-6"
-				:cost="70"
-				description="Приобрети коины сразу и ни в чём себе не отказывай!"
-				:button="{ to: Link.Buy, color: ButtonColor.Yellow, text: 'Купить' }"
-			>
-				<p>10 решений</p>
-			</AppCardPrice>
+      Наше расширение для <b>ответов</b> на задания <b>UzTest</b> больше не поддерживается.
+      <br><br>
+      Но вы можете заказать решение на <a class="link text-aqua" href="https://domashky.net/" target="_blank">Domashky.NET</a>
+      и получить <span class="text-yellow">50 рублей</span> при регистрации (только для первых 100 пользователей)
 		</template>
 		<template #keywords>
 			uztest ответы, uztest взлом, гдз uztest, решение uztest, как взломать uztest, как сделать быстро юзтест,
